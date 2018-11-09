ef132c214136969038adebebf6793696da77caaf
Hayquan <jsxzfxhhq@qq.com>
2018-11-09 18:38:04 +0800

    get wifi ssid

--- app/src/main/java/com/wonium/example/MainActivity.java
+++ app/src/main/java/com/wonium/example/MainActivity.java
@@ -1,17 +1,19 @@
 package com.wonium.example;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.TextView;
 
 import com.wonium.extension.utils.DeviceUtil;
 
-public class MainActivity extends AppCompatActivity {
+import ru.alexbykov.nopermission.PermissionHelper;
 
+public class MainActivity extends AppCompatActivity {
+    PermissionHelper permissionHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,11 +23,29 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show());
+         permissionHelper = new PermissionHelper(this);
+        XXXX();
     }
+
+    private void XXXX(){
+       //don't use getActivity in fragment!
+
+        permissionHelper.check(Manifest.permission.ACCESS_FINE_LOCATION)
+                .onSuccess(this::onSuccess)
+                .onDenied(this::onDenied)
+                .onNeverAskAgain(this::onNeverAskAgain)
+                .run();
+    }
+    private void onSuccess(){
+        TextView tvSSID =findViewById(R.id.tvSSID);
+        tvSSID.setText(DeviceUtil.INSTANCE.getWIFISSID(this));
+    }
+    private void onDenied(){
+
+    }
+    private void onNeverAskAgain(){
+
+    }
+
 }
